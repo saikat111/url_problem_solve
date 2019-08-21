eae92bae54901b59b71791a49afe0d69a5c5fd2c
al rakib sorder <46457169+saikat111@users.noreply.github.com>
2019-08-21 11:39:42 -0700

    1078_no._problem

--- main.c
+++ main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-
 int main()
 {
- int n;
- scanf("%d",&n);
-
- for(int i=1;i<=10000;i++)
- {
-     if(n%i==2)
-     {
-
-         printf("%d\n", i);
-     }
-
- }
-
+int n,i;
+scanf("%d",&n);
+for(i=1;i<=10;i++)
+{
+    printf("%d x %d = %d\n",i,n,i*n);
+}
+    return 0;
 }
